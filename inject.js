bc1d87abe56ac1571e7188d184432dae94d7497d
Chris Bujok <chris.bujok@gmail.com>
2016-08-31 22:24:39 +0200

    Recursively detect A node in case of target set to inner element

--- inject.js
+++ inject.js
@@ -1,3 +1,15 @@
+function recursive(node, i) {
+  if (node.nodeName === 'A') {
+    return node;
+  }
+
+  if (i < 4) {
+    return recursive(node.parentNode, i + 1);
+  }
+
+  return null;
+}
+
 document.addEventListener(
   'webkitmouseforcewillbegin',
   function(event) {
@@ -10,10 +22,12 @@ document.addEventListener(
 document.addEventListener(
   'webkitmouseforcedown',
   function(event) {
-    if (event.target.nodeName === 'A') {
+    var node = recursive(event.target, 0);
+
+    if (node) {
       safari.self.tab.dispatchMessage(
         'open-new-tab',
-        event.target.href
+        node.href
       );
     }
   }
